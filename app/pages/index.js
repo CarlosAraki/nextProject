e6bec11ea5395746672cf71151a0665bc4eeb118
Carlos <carlosarak@gmail.com>
2020-12-01 22:48:36 -0300

    barh

--- app/pages/index.js
+++ app/pages/index.js
@@ -3,7 +3,7 @@ import {useState} from "react"
 function Home (){
     return(
             <div>
-                <h1> Oi mozao Linda acabei de fazer um deploy Full </h1>
+                <h1> Full Barh </h1>
                 <Contador/>
             </div>
         ) 
