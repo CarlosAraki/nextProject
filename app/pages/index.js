64184e6e49db8f5ea4e3fd94e872982dcaa691a5
Carlos <carlosarak@gmail.com>
2020-12-01 22:28:33 -0300

    feat: teste de deploy

--- app/pages/index.js
+++ app/pages/index.js
@@ -3,7 +3,7 @@ import {useState} from "react"
 function Home (){
     return(
             <div>
-                <h1> Home </h1>
+                <h1> Oi mozao Linda acabei de fazer um deploy Full </h1>
                 <Contador/>
             </div>
         ) 
